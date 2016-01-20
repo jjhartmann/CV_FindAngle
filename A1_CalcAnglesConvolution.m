6f9c17c04df6ec42388b06d7e6441bf1938126c8
jhartman <jhartman@sfu.ca>
2016-01-20 13:17:20 -0800

    Add, threshold to convert image to a block of [-1, -]

--- A1_CalcAnglesConvolution.m
+++ A1_CalcAnglesConvolution.m
@@ -9,3 +9,11 @@ imshow(img)
 bw = double(double(img(:,:,1)) + double(img(:, :, 2)) + double(img(:, :, 3)))/(3*255);
 imshow(bw);
 
+% TODO: Smoothing of the image. Needs to promote the pencils more. 
+% convert image to a threshold black and white image
+th = 110/255;
+th_image = -ones(size(bw)); % create a black image with size of bw
+th_index = find(bw<th); % Finds all the index that satisfy the threshold
+th_image(th_index) = 1; % Takes all the indices and assigns one to them, creating a black and white file from the gray scale. 
+
+imshow(th_image)
