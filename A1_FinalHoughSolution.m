bbacb80cd422eeb3a317e5e753d748e8f6f073cf
jhartman <jhartman@sfu.ca>
2016-02-01 23:57:49 -0800

    Fix, Clean, and Polish
    Fix and align print output.
    - Add proper angle calculations
    - Remove legacy code.
    - Improve comments

--- A1_FinalHoughSolution.m
+++ A1_FinalHoughSolution.m
@@ -23,18 +23,18 @@ end
 img = imread(image_text);
 imshow(img)
 
-%% Convert RGB image to HVS
-RGB = im2double(img);
-cform = makecform('srgb2lab', 'AdaptedWhitePoint', whitepoint('D65'));
-I = applycform(RGB,cform);
-
 %% Determine if the image is light or dark
-GrayImageM = rgb2gray(RGB);
+GrayImageM = rgb2gray(img);
 imgMedian = median(GrayImageM(:));
 
-if (imgMedian < 0.55)
+if (imgMedian < 150)
 %% Define thresholds for channel 1 based on histogram settings
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Convert RGB image to LAB
+    RGB = im2double(img);
+    cform = makecform('srgb2lab', 'AdaptedWhitePoint', whitepoint('D65'));
+    I = applycform(RGB,cform);
+
     channel1Min = 13.262;
     channel1Max = 67.444;
 
@@ -63,6 +63,7 @@ if (imgMedian < 0.55)
 else
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Apply gaussian smoothing
+    RGB = im2double(img);
     img = imgaussfilt3(img, 2);
     imshow(img)
 
@@ -99,56 +100,19 @@ else
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
 
-
+%% Convert to gray scale mask
 maskedGray = rgb2gray(maskedRGBImage);
 imshow(maskedGray)
-
-%% %convert the image to a gray scale image
-% bw = double(1.2 * double(img(:,:,1)) + 1.2 * double(img(:, :, 2)) + .2 * double(img(:, :, 3)))/(3*255);
-% imshow(bw);
-
-% Convert to binary image
-% Threshold
-% th = 100;
-% th_image = im2bw(maskedGray, th/255);
-% imshow(th_image)
-% 
-% % Clean up image based on segments
-% th_image = bwareaopen(th_image, 100);
-% imshow(th_image)
-% 
-% % Clean the border of the image from artifacts. 
-% th_image = imclearborder(th_image);
-% imshow(th_image)
-
-% % TODO: Smoothing of the image. Needs to promote the pencils more. 
-% % laplacian of gaussian
-% h = fspecial('log',15,3);
-% bg=filter2(h, th_image);
-% surf(h);  %Display the mask
-% %Can't display the result of the convolution directly because it has
-% %negative values.
-% %Add an offset and then scale to 0-1 range for display purposes
-% bgs=bg+.5;
-% bgs=bgs-min(bgs(:));
-% bgs=bgs/max(bgs(:));
-% imshow(bgs);
 
 %% Find the edges of the image using laplacian of gaussian. 
 imgedg = edge(maskedGray, 'log');
 imshow(imgedg)
-
-maskedGray(find(maskedGray)) = 1;
 
 smooth = imdilate(imgedg,strel('disk',1));
 imshow(smooth)
 
 %% Find Regions and display
 [B, L] = bwboundaries(smooth, 'holes');
-
-%% TEST
-% L = maskedGray;
-%%
 
 imshow(label2rgb(L))
 
@@ -165,38 +129,19 @@ imshow(L)
 %% Thin the image
 L_thin = bwmorph(L, 'thin', inf);
 imshow(L_thin)
-imgedg = L_thin;
-
-%% % Find shapes based on eccentricity
-% stats = regionprops(L, 'all');
-% shapes = [stats.Eccentricity];
-% pencils_index = find(shapes > 0.98);
-% 
-% 
-% % Location of pencils
-% pencils = stats(pencils_index);
-% locations = [pencils.Centroid];
-% 
-% 
-% figure;
-% imshow(th_image); hold on;
-% for k = 1:(length(locations)/2)
-%     plot(locations((k*2)-1), locations((k*2)), 'x', 'LineWidth', 2, 'Color', 'red'); 
-% end
-
 
 %% Crop the outer ridges of the image. 
-[width, height] = size(imgedg);
-imgedg = imcrop(imgedg,[30,30,height-60, width-60]);
+[width, height] = size(L_thin);
+L_thin = imcrop(L_thin,[30,30,height-60, width-60]);
 img = imcrop(img,[30,30,height-60, width-60]);
-imshow(imgedg)
+imshow(L_thin)
 
 %smooth image
-imgedg = imdilate(imgedg,strel('disk',1));
-imshow(imgedg)
+L_smooth = imdilate(L_thin,strel('disk',1));
+imshow(L_smooth)
 
 %% Compute hough transform. 
-[H, theta, rho] = hough(imgedg);
+[H, theta, rho] = hough(L_smooth);
 
 % Display transform data
 figure, imshow(imadjust(mat2gray(H)), [], 'XData', theta, 'YData', rho,'InitialMagnification', 'fit');
@@ -214,12 +159,16 @@ y = rho(peaks(:,1));
 plot(x,y,'s', 'color', 'black')
  
 %% Find lines using houghlines
-lines = houghlines(imgedg, theta, rho, peaks, 'FillGap', 30, 'MinLength', 100);
+lines = houghlines(L_smooth, theta, rho, peaks, 'FillGap', 30, 'MinLength', 100);
 
 %% Eliminate lines that are simlar. ie some epsilon difference between them.
 for k = 1:length(lines)
     deltaPoint = lines(k).point2 - lines(k).point1;
     lines(k).Length = sqrt(deltaPoint(1)^2 + deltaPoint(2)^2);
+    
+    % Create angles in degrees of lines
+    lines(k).LineDegree = (atan(double(deltaPoint(2)/deltaPoint(1))) * double(180/pi)) + 90;
+    
 end
 
 % Sort by length
@@ -292,7 +241,6 @@ end
 %% Find orientation
 
 
-
 %% Plot lines on original image. 
 figure, imshow(img), hold on
 max_len = 0 % Todo: expand for the top n lines
@@ -314,16 +262,18 @@ for k = 1:length(lines)
         xy_long = xy;
     end
     
-    % Print output
-    disp(['Pencil ', num2str(k) ':'])
-    disp(['Length: ', num2str(lines(k).Length)])
-    disp(['Angle: ', num2str(lines(k).theta)])
-    
     % find position
     Lmidpoint = [(xy(1,1) + xy(2,1))/2, (xy(1,2) + xy(2,2))/2];
     plot(Lmidpoint(1), Lmidpoint(2), 'o', 'LineWidth', 2, 'Color', 'blue');
-   
-    disp(['Position: [', num2str(Lmidpoint(1)), ', ', num2str(Lmidpoint(2)), ']'])
+    
+    % Print output
+    disp(['Pencil ', num2str(k) ':'])
+    disp(['  Length:          ', num2str(lines(k).Length)])
+    disp(['  Angle:           ', num2str(atan((lines(k).point2(1) - Lmidpoint(1))/(lines(k).point2(2) - Lmidpoint(2))) * (180/pi) + 90)])
+    disp(['  Center Position: [', num2str(Lmidpoint(1)), ', ', num2str(Lmidpoint(2)), ']'])
+    disp(['  End Position:    [', num2str(lines(k).point1), ']'])
+    disp(['  Begin Position:  [',  num2str(lines(k).point2), ']'])
+
     disp(' ')
     
 end
